e7e6bdbae05d6399f69cbd32ff8bf2c195367053
ShehanSH <shehanhashen928@gmail.com>
2023-07-17 18:22:16 +0530

    Initial commit

---
+++ CRUSApplication/src/test/java/com/example/CRUSApplication/CrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUSApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
